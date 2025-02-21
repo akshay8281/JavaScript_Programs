177836aa10adb1bc229e0789afc95bc72cbdacb6
Akshay8281 <aks.pitroda.8@gmail.com>
2025-02-21 22:37:14 +0530

    Digital CLock

---
+++ Extra/03_DigiClock/app.js
@@ -0,0 +1,9 @@
+const clock = document.querySelector(".clock")
+
+
+setInterval(() => {
+    let date = new Date()
+    clock.innerHTML = date.toLocaleTimeString()
+    // console.log(date.toLocaleTimeString());
+    
+}, 1000);
